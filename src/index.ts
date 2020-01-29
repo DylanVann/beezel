9f7344e6af7195e3b0c53fd427a5b2ceff00fa11
Dylan Vann <dylan@dylanvann.com>
2020-01-29 18:01:45 -0500

    fix version log

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@
 import "hard-rejection/register"
 import { syncPackages } from "./syncPackages"
 import fs from "fs-extra"
+import path from "path"
 import { cacheDir } from "paths"
 
 const run = async () => {
-  console.log(`Beezel - v0.0.x`)
+  const pkg = require(path.join(__dirname, "..", "package.json"))
+  console.log(`Beezel - v${pkg.version}`)
   await fs.ensureDir(cacheDir)
   await syncPackages()
 }
