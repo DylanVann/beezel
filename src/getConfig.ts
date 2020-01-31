f2549e721f4eb5d255f072f0412442ea66df30b6
Dylan Vann <dylan@dylanvann.com>
2020-01-31 17:46:08 -0500

    remove cosmiconfig

--- src/getConfig.ts
+++ src/getConfig.ts
@@ -1,10 +1,11 @@
-import { cosmiconfig } from 'cosmiconfig'
 import { root } from './paths'
+import path from 'path'
+import fs from 'fs-extra'
 
 export const getConfig = async (): Promise<{
   otherYarnCaches?: string[]
   globalDependencies: string[]
 }> => {
-  const configResult = await cosmiconfig('beezel').search(root)
-  return (configResult || {}).config || {}
+  const pkg = await fs.readJson(path.join(root, 'package.json'))
+  return pkg.beezel || {}
 }
