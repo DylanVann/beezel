3e0284d50e9c6e82e98e352e9a6c5234f483f230
Dylan Vann <dylan@dylanvann.com>
2020-01-31 15:51:08 -0500

    use global hash in yarn hash

--- src/getYarnHash.ts
+++ src/getYarnHash.ts
@@ -1,13 +1,14 @@
 import { getGitHashForFiles } from '@microsoft/package-deps-hash'
 import objectHash from 'object-hash'
-import { env } from './env'
 import { root } from './paths'
 import memoize from 'lodash.memoize'
+import { getGlobalHash } from 'getGlobalHash'
 
 // Key for yarn.lock
 export const getYarnHash = memoize(
   async (): Promise<string> => {
+    const globalHash = await getGlobalHash()
     const hashMap = getGitHashForFiles(['yarn.lock'], root)
-    return `yarn-${objectHash([hashMap, env.BEEZEL_CACHE_KEY])}.tgz`
+    return `yarn-${objectHash([hashMap, globalHash])}.tgz`
   },
 )
