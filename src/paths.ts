9cd0ab4caa7919aad3bf941055525155e580935f
Dylan Vann <dylan@dylanvann.com>
2020-03-23 17:32:56 -0400

    use cwd for finding root

--- src/paths.ts
+++ src/paths.ts
@@ -1,7 +1,7 @@
 import findWorkspaceRoot from 'find-yarn-workspace-root'
 
 const getRoot = (): string => {
-  const root = findWorkspaceRoot()
+  const root = findWorkspaceRoot(process.cwd())
   if (!root) {
     throw new Error('Could not find workspace root.')
   }
